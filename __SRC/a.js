cd4e99cf16cd4ddc23f7db019c1b6dc4db5746f0
termi <termi_uc@inbox.ru>
2012-07-22 01:10:55 +0400

    a.js: hotfix

--- __SRC/a.js
+++ __SRC/a.js
@@ -2416,9 +2416,9 @@ function extendNodeListPrototype(nodeList_proto) {
 	//in old FF nodeList_proto.__proto__ != nodeList_proto.constructor.prototype
 	nodeList_proto = nodeList_proto.__proto__ || nodeList_proto.constructor.prototype;
 
-	if(nodeListProto && nodeListProto !== Array.prototype) {
+	if(nodeList_proto && nodeList_proto !== Array.prototype) {
 		nodeList_methods_fromArray.forEach(function(key) {
-			if(!nodeListProto[key])nodeListProto[key] = Array.prototype[key];
+			if(!nodeList_proto[key])nodeList_proto[key] = Array.prototype[key];
 		})
 	}
 }
